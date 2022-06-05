df1d4c0b30370af0166c99ef412dccebd2c03318
Fayha Syifa Qalbi <72442637+sfayhaa@users.noreply.github.com>
2022-06-05 20:10:52 +0700

    Add files via upload

---
+++ PBKK-4/Supermarket/ItemPenjualan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Supermarket
+{
+    public class ItemPenjualan
+    {
+        public ItemPenjualan()
+        {
+            DiskonPersen = 0;
+        }
+
+        public long Id { get; set; }
+
+        public string NamaBarang { get; set; }
+
+        public int Jumlah { get; set; }
+
+        public decimal Harga { get; set; }
+
+        public decimal DiskonPersen { get; set; }
+
+        public decimal Total()
+        {
+            decimal total = Jumlah * Harga;
+            return total - (DiskonPersen / 100 * total);
+        }
+    }
+}
---
+++ PBKK-4/Supermarket/ItemPenjualanViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Supermarket
+{
+    class ItemPenjualanViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ItemPenjualan model;
+
+        public ItemPenjualanViewModel(ItemPenjualan itemPenjualan = null)
+        {
+            this.model = itemPenjualan ?? new ItemPenjualan();
+        }
+
+        public string NamaBarang
+        {
+            get { return model.NamaBarang; }
+            set
+            {
+                if (value != model.NamaBarang)
+                {
+                    model.NamaBarang = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("NamaBarang"));
+                }
+            }
+        }
+
+        public int Jumlah
+        {
+            get { return model.Jumlah; }
+            set
+            {
+                if (value != model.Jumlah)
+                {
+                    model.Jumlah = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Jumlah"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("Total"));
+                }
+            }
+        }
+
+        public decimal Harga
+        {
+            get { return model.Harga; }
+            set
+            {
+                if (value != model.Harga)
+                {
+                    model.Harga = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Harga"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("Total"));
+                }
+            }
+        }
+
+        public decimal DiskonPersen
+        {
+            get { return model.DiskonPersen; }
+            set
+            {
+                if (value != model.DiskonPersen)
+                {
+                    model.DiskonPersen = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("DiskonPersen"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("Total"));
+                }
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                decimal? total = model.Total();
+                if (!total.HasValue)
+                {
+                    return "-";
+                }
+                else
+                {
+                    return total.Value.ToString("C");
+                }
+            }
+        }
+
+        public ItemPenjualan Model
+        {
+            get { return this.model; }
+        }
+    }
+}
